2bab706cd6a4bcd858bf64bf7007527c77c5608f
Brahim <bjaayfer@gmail.com>
2025-05-04 15:37:37 +0200

    Initial project setup with basic Express server and tests

---
+++ src/server.js
@@ -0,0 +1,28 @@
+// src/server.js
+
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res)=>{
+    res.render('index', { title :'Video Transcription App'})
+    //res.send('Video Transcription App is running!')
+})
+
+// we just export the app when testing
+if(process.env.NODE_ENV !== 'test'){
+    app.listen(PORT, ()=>{
+        console.log(`Server running on http://localhost:${PORT}`)
+    })
+}
+
+module.exports = app;
